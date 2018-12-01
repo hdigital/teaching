b43699fd95afb583d24ecf005a1dd621d8e22fac
Holger Döring <doering@uni-bremen.de>
2018-12-01 14:08:01 +0100

    add Twitter R courses examples

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 Holger Döring -- University of Bremen -- doering@uni-bremen.de
 
-[Parteien und Parteiensystem in Deutschland](parteien-deutschland-2018/) (2018, German)
-
+- [Parteien und Parteiensystem in Deutschland](parteien-deutschland-2018/) (2018, German)
+- [Twitter](twitter/) example used to motivate R courses
 
 ![Wahlen-in Deutschland](parteien-deutschland-2018/wid-ergebnisse.png)
---
+++ twitter/twitter-simple.R
@@ -0,0 +1,45 @@
+library(tidyverse)
+library(twitteR)
+library(quanteda)
+
+
+# Authentication Twitter ----
+
+# ADD YOUR PERSONAL TWITTER API ACCESS (OAuth access token)
+# see https://dev.twitter.com/oauth/overview/application-owner-access-tokens
+options(httr_oauth_cache = FALSE)  # skip required user input next line
+twitteR::setup_twitter_oauth(
+  "YOUR_CONSUMER_KEY",
+  "YOUR_CONSUMER_SECRET",
+  "YOUR_ACCESS_TOKEN",
+  "YOUR_ACCESS_TOKEN_SECRET"
+)
+
+
+# Search Twitter ----
+
+search_term <- "brexit"
+tweets <- twitteR::searchTwitter(search_term, n = 250)
+# tweets <- twitteR::userTimeline(search_term, n = 250)
+tw_df <- twListToDF(tweets)
+
+
+# Text analysis ----
+
+tw_cp <- corpus(
+  tw_df %>% select(id, created, text), 
+  text_field = "text",
+  docvars = "created"
+  )
+
+tw_dfm <- 
+  tw_cp %>% 
+  tokens(remove_punct = TRUE, remove_twitter = TRUE) %>% 
+  tokens_remove(phrase(c(stopwords("en"), search_term, "http*", "news", "rt", "t.co"))) %>% 
+  dfm()
+
+
+# Visualize results ----
+
+textplot_wordcloud(tw_dfm)
+
