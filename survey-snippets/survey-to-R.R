f88da523e7208dcbacae4a7ae885e038a099cfea
hdigital <doering@uni-bremen.de>
2023-04-03 11:35:24 +0200

    Update survey snippet (#1)
    * Trim trailing whitespace
    * Use Base-R pipe
    * Use Base-R anonymous function
    * Remove variable format column
    * Update example to ALLBUS 2021

--- survey-snippets/survey-to-R.R
+++ survey-snippets/survey-to-R.R
@@ -9,8 +9,8 @@ negative_numbers_into_NA <- TRUE
 ## Read data ----
 
 # prefer SPSS with labelled data -- previously problems with Stata encodings and missings
-file_source <- "source__ZA5270/ZA5270_v2-0-0.sav.zip"
-files_out <- "source__Allbus-2018"
+file_source <- "source__ZA5280/ZA5280_v2-0-0.sav"
+files_out <- "source__Allbus-2021"
 
 svy_format <- case_when(
   str_detect(file_source, "dta") ~ "stata",
@@ -27,23 +27,22 @@ if(svy_format == "spss") {
 
 ## Variable information ----
 
-svy_var <- 
+svy_var <-
   tibble(
     variable = names(raw_svy),
-    tmp_1 = map(variable, ~ attributes(raw_svy[[.x]])[["label"]]),
-    label = map_chr(tmp_1, ~ if_else(is.null(.x), "", .x)),
-    tmp_2 = map(names(raw_svy), ~ attr(raw_svy[[.x]], svy_format)),
-    format = map_chr(tmp_2, ~ if_else(is.null(.x), "", .x)),
-    factor = map_int(variable, ~ attributes(raw_svy[[.x]])[["labels"]] %>% max() > 0 )
-  ) %>% 
-  select(-tmp_1, -tmp_2)
+    tmp_1 = map(variable, \(.x) attributes(raw_svy[[.x]])[["label"]]),
+    label = map_chr(tmp_1, \(.x) if_else(is.null(.x), "", .x)),
+    # format = map(names(raw_svy), \(.x) typeof(raw_svy[[.x]])),
+    factor = map_int(variable, \(.x) attributes(raw_svy[[.x]])[["labels"]] |> max() > 0 )
+  ) |>
+  select(-tmp_1)
 
 
 ## Variable labels ----
 
 get_svy_labels <- function(svy_var) {
   labels <- attr(pull(raw_svy, svy_var), "labels")
-  
+
   if(! is.null(labels)) {
     tibble(variable = svy_var, label = names(labels), value = as.character(labels))
   } else {
@@ -57,9 +56,9 @@ svy_label <- map_df(names(raw_svy), get_svy_labels)
 ## R data formats ----
 
 # get names of factor variables
-to_factor <- 
-  svy_var %>% 
-  filter(factor == 1) %>% 
+to_factor <-
+  svy_var |>
+  filter(factor == 1) |>
   pull(variable)
 
 svy_dt <- raw_svy
@@ -70,9 +69,9 @@ if(negative_numbers_into_NA == TRUE) {
 }
 
 # remove unused factor levels not used and variable labels
-svy_dt <- 
-  svy_dt %>% 
-  mutate(across(any_of(to_factor), ~ as_factor(.x) %>% fct_drop())) %>%
+svy_dt <-
+  svy_dt |>
+  mutate(across(any_of(to_factor), \(.x) as_factor(.x) |> fct_drop())) |>
   zap_labels()
 
 
